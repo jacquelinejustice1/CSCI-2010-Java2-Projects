5a148a54a16757a576c29f7262aa1fe023427c06
Jacqueline Justice <158090634+jacquelinejustice1@users.noreply.github.com>
2024-08-25 13:31:14 -0400

    Add files via upload

---
+++ Assignment 5/JusticeProgram5.java
@@ -0,0 +1,32 @@
+
+package justiceprogram5;
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+import javafx.scene.Scene;
+import javafx.scene.Group;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.ArcType;
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author Jacqueline Justice
+ */
+public class JusticeProgram5 extends Application {
+    
+    @Override
+    public void start(Stage stage){
+        
+    }
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+}
